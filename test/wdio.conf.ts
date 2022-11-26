93e09aff07c61aedfb0b50bffaf2745170a7cef4
Paul Grossman <TheDarkArtsWizard@gmail.com>
2022-11-26 03:47:09 -0600

    Revert "Ignore logs"
    This reverts commit 7fed328907d59022edc74a8687c5adc9d2c7436f.

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -79,7 +79,7 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-
+    
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
@@ -139,16 +139,8 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-
-    outputDir: 'all-logs',
-    services: [
-        ['chromedriver', {
-            logFileName: 'wdio-chromedriver.log', // default 
-            outputDir: 'driver-logs', // overwrites the config.outputDir 
-            args: ['--silent']
-        }]
-    ],
-
+    services: ['chromedriver'],
+    
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
@@ -169,18 +161,10 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
+    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+
+
     
-
-    reporters: ['spec',['allure', { 
-
-        outputDir: 'allure-results', 
-
-        disableWebdriverStepsReporting: true, 
-
-        disableWebdriverScreenshotsReporting: true, 
-
-    }]], 
-
     //
     // Options to be passed to Jasmine.
     jasmineOpts: {
@@ -190,12 +174,11 @@ export const config: Options.Testrunner = {
         // The Jasmine framework allows interception of each assertion in order to log the state of the application
         // or website depending on the result. For example, it is pretty handy to take a screenshot every time
         // an assertion fails.
-        expectationResultHandler: function (passed, assertion)
-        {
+        expectationResultHandler: function(passed, assertion) {
             // do something
         }
     },
-
+    
     //
     // =====
     // Hooks
@@ -290,10 +273,8 @@ export const config: Options.Testrunner = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    afterTest: async function (test, context, { error, result, duration, passed, retries })
-    {
-        if (!passed)
-        {
+    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        if (!passed) {
             await browser.takeScreenshot();
         }
     },
