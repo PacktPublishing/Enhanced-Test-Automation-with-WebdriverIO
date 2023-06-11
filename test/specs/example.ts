dd00443b7c8cc228e33c95705d3910a134bc58be
Paul Grossman <thedarkartswizard@gmail.com>
2023-06-11 05:38:57 -0500

    Clean up from Chapter 2

--- test/specs/example.ts
+++ test/specs/example.ts
@@ -4,14 +4,13 @@ import SecurePage from "../pageobjects/secure.page";
 
 describe("My Login application", () => {
   it("should login with valid credentials", async () => {
-    await LoginPage.open();
-    // Chapter 3
-    // console.log (`Entering password`) // Intrinsic log
-    // await global.log (`Entering password`) // Custom log
-    // await global.log (``) // Does not print
-    // await global.log (null) // Does not print
-    // await global.log (Promise) // Adds trace back
 
+
+
+
+    
+
+    await LoginPage.open();
     LoginPage.login("tomsmith", "SuperSecretPassword!");
     expect(SecurePage.flashAlert).toBeExisting();
     expect(SecurePage.flashAlert).toHaveTextContaining(
@@ -19,3 +18,4 @@ describe("My Login application", () => {
     );
   });
 });
+
