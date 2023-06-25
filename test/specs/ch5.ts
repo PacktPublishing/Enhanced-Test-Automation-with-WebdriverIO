69e1e27cfdb95ecaed2472876fcebc5271089558
Paul Grossman <thedarkartswizard@gmail.com>
2023-06-25 04:56:28 -0500

    Update from ch4

--- test/specs/ch5.ts
+++ test/specs/ch5.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "strict mode";
 import LoginPage from "../pageobjects/login.page.js";
 import SecurePage from "../pageobjects/secure.page.js";
@@ -102,25 +101,3 @@ describe("My Login application", () => {
     );
   });
 });
-=======
-import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
-
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        LoginPage.login('tomsmith1', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
-
-        // Chapter 4
-        Promise.resolve().then( _ => console.log(`2`));
-        setTimeout( _ => console.log(`3`),0);
-        console.log(`1`);
-    });
-});
-
-
->>>>>>> chapter-4
