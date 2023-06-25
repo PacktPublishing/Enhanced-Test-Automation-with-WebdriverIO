02e47a13ecdcf56194018077a60e3d2037148e6f
Paul Grossman <thedarkartswizard@gmail.com>
2023-06-25 04:06:41 -0500

    Added ch3
    removed fit

--- test/specs/ch1.ts
+++ test/specs/ch1.ts
@@ -2,7 +2,7 @@ import LoginPage from "../pageobjects/login.page";
 import SecurePage from "../pageobjects/secure.page";
 
 describe("Ch1: Utility Belt", () => {
-  fit("should login with valid credentials", async () => {
+  it("should login with valid credentials", async () => {
     await LoginPage.open();
 
     await LoginPage.login("tomsmith", "SuperSecretPassword!");
--- test/specs/ch2.ts
+++ test/specs/ch2.ts
@@ -2,7 +2,7 @@ import LoginPage from "../pageobjects/login.page";
 import SecurePage from "../pageobjects/secure.page";
 
 describe("Ch2: Fortress of Solitude", () => {
-  fit("should login with valid credentials", async () => {
+  it("should login with valid credentials", async () => {
     await LoginPage.open();
 
     await LoginPage.login("tomsmith", "SuperSecretPassword!");
