2dd25709d208aab52699ceaa3c6a2a2715867c66
Paul Grossman <thedarkartswizard@gmail.com>
2023-06-25 03:39:21 -0500

    added Ch2 script

--- test/specs/ch2.ts
+++ test/specs/ch2.ts
@@ -1,7 +1,7 @@
-import LoginPage from "../../pageobjects/login.page";
-import SecurePage from "../../pageobjects/secure.page";
+import LoginPage from "../pageobjects/login.page";
+import SecurePage from "../pageobjects/secure.page";
 
-describe("Ch2: My Login application", () => {
+describe("Ch2: Fortress of Solitude", () => {
   fit("should login with valid credentials", async () => {
     await LoginPage.open();
 
