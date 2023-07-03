8f2ec448681a7eaaabce2aed56732b69917252e4
Paul Grossman <thedarkartswizard@gmail.com>
2023-07-03 02:38:12 -0500

    Restore await to example

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,16 +1,16 @@
 import LoginPage from  '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
 
-describe('My Login application', () => {
+describe("My Login application", () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
-        LoginPage.login('tomsmith1', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
-
     });
 });
+
 
 
