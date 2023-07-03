b5414212e41acc55d6e05a078f258643b13d559a
Paul Grossman <thedarkartswizard@gmail.com>
2023-07-03 02:40:18 -0500

    add await to setwindow

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -273,7 +273,7 @@ export const config: Options.Testrunner = {
     // Option #2: Run browser 3/4 screen on single monitor
     // Allow VS Code Terminal visible on bottom of the screen
     await global.log(`Changing window size`);
-    browser.setWindowSize(1920, 770);
+    await browser.setWindowSize(1920, 770);
   },
   /**
    * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
