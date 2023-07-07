7daf30347563553005ad75594a830e082c238f00
LarryG <larryg@klassitech.co.uk>
2023-07-07 20:04:17 +0100

    chore(): code clean up

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,3 +1,4 @@
+import { Browser } from "webdriverio";
 import type { Options } from "@wdio/types";
 
 const DEBUG =
@@ -7,7 +8,7 @@ console.log(`DEBUG: ${DEBUG}`);
 let timeout = DEBUG === true ? 1_000_000 : 10_000;
 console.log(`timeout = ${Math.ceil(timeout / 60_000)} min.`);
 
-export const config: Options.Testrunner = {
+export const config = {
   //
   // ====================
   // Runner Configuration
@@ -83,21 +84,15 @@ export const config: Options.Testrunner = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-  capabilities: [
-    {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      maxInstances: 5,
-      //
-      browserName: "chrome",
-      acceptInsecureCerts: true,
-      // If outputDir is provided WebdriverIO can capture driver session logs
-      // it is possible to configure which logTypes to include/exclude.
-      // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-      // excludeDriverLogs: ['bugreport', 'server'],
-    },
-  ],
+  capabilities: [{
+    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    // grid with only 5 firefox instances available you can make sure that not more than
+    // 5 instances get started at a time.
+    browserName: 'chrome', // or "firefox", "microsoftedge", "safari"
+    'goog:chromeOptions': {
+      args: ['--disable-gpu', '--enable-automation', '--disable-infobars', '--disable-notifications'] },
+    acceptInsecureCerts: true,
+  }],
   //
   // ===================
   // Test Configurations
@@ -282,9 +277,9 @@ export const config: Options.Testrunner = {
    * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
    * beforeEach in Mocha)
    */
-  beforeHook: function (test, context) {
-    // Create custom commands here
-  },
+  // beforeHook: function (test, context) {
+  //   // Create custom commands here
+  // },
   /**
    * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
    * afterEach in Mocha)
