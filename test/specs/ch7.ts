a03ff7a01f18f145dc0f7ad5ad5ace2c060722be
Paul Grossman <thedarkartswizard@gmail.com>
2023-07-14 03:09:34 -0500

    Create ch7.ts

---
+++ test/specs/ch7.ts
@@ -0,0 +1,22 @@
+"strict mode";
+import LoginPage from "../pageobjects/login.page";
+import SecurePage from "../pageobjects/secure.page";
+import LandingPage from "../pageobjects/landing.page";
+import * as helpers from "../../helpers/helpers";
+import dynamicLoadingPage from "../pageobjects/dynamicLoading.page";
+describe("The setValue Wrapper – Entering Text and Dynamic Data Replacement", () => {
+
+    fit("Chapter 6: setValue Wrapper", async () => {
+        // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
+    
+        console.log(await helpers.getToday()); // returns current date in MM-dd-yyyy format
+        console.log(await helpers.getToday(7, "d/M/yyyy")); // returns current date plus 5 days in d/M/yyyy format
+        console.log(await helpers.getToday(-30, "yyyy/MM/dd")); // returns current date minus 3 days in yyyy/MM/dd format
+    
+        await LoginPage.open();
+        await LoginPage.loginSetValue("tomsmith", "SuperSecretPassword!");
+    
+        await LoginPage.open("key_presses");
+        await KeyPressesPage.enterDateTags();
+      });
+  });
