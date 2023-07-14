3aa0648e8bed62cf495ada898d495bf631942078
Paul Grossman <thedarkartswizard@gmail.com>
2023-07-14 02:28:50 -0500

    Restored ch6

--- test/specs/ch5.ts
+++ test/specs/ch5.ts
@@ -1,44 +1,18 @@
 "strict mode";
-import LoginPage from "../pageobjects/login.page.js";
-import SecurePage from "../pageobjects/secure.page.js";
-import LandingPage from "../pageobjects/landing.page.js";
-import KeyPressesPage from "../pageobjects/Inputs.page.js";
-import TyposPage from "../pageobjects/typos.page.js";
-import * as helpers from "../../helpers/helpers.js";
-import dynamicLoadingPage from "../pageobjects/dynamicLoading.page.js";
-
-describe("My Login application", () => {
-  it("Chapter 3:Cybernetic Enhancements Custom Logging", async () => {
-    // Chapter 3
-    console.log(`Hello, World!`); // Intrinsic log
-    await helpers.log(`Hello, World!`); // Custom log
-    await helpers.sleep(1000); // Wait 1 sec
-    await helpers.log(`DONE!`); // Custom log
-  });
-
-  it("Chapter 4: Super Speed - Time Travel Paradoxes and Broken Promises", async () => {
-    await helpers.log(`Hello, World!`);
-    await helpers.log(
-      `PASS: You are never too old to set another goal or to dream a new dream. -- C.S. Lewis`
-    );
-    await helpers.log(`FAIL: It's never too late to go back to bed.`);
-    await helpers.log(`WARN: Earth is mostly harmless`);
-    await helpers.log(``); // Does not print
-    await helpers.log(null); // Does not print
-    await helpers.log(Promise); // Adds trace back
-
-    // Promise.resolve().then( _ => console.log(`2`));
-    // setTimeout( _ => console.log(`3`),0);
-    // console.log(`1`);
-  });
+import LoginPage from "../pageobjects/login.page";
+import SecurePage from "../pageobjects/secure.page";
+import LandingPage from "../pageobjects/landing.page";
+import * as helpers from "../../helpers/helpers";
+import dynamicLoadingPage from "../pageobjects/dynamicLoading.page";
+describe("Chapter 5: Fail Last clickAdv with pageSync and autoscrolln", () => {
 
   it("Chapter 5: Fail Last clickAdv with pageSync and autoscroll", async () => {
     // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
     await LoginPage.open();
 
     await LoginPage.loginFailLast("tomsmith", "SuperSecretPassword!");
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveTextContaining(
+    await expect(await SecurePage.flashAlert).toBeExisting();
+    await expect(await SecurePage.flashAlert).toHaveTextContaining(
       "You logged into a secure area!"
     );
   });
@@ -71,9 +45,9 @@ describe("My Login application", () => {
     await expect(dynamicLoadingPage.txtHelloWorld).toHaveTextContaining(
       "Hello World"
     );
-    await helpers.highlightOn(dynamicLoadingPage.txtHelloWorld);
+    await helpers.highlightOn(await dynamicLoadingPage.txtHelloWorld);
     await helpers.pause(5000);
-    await helpers.highlightOff(dynamicLoadingPage.txtHelloWorld);
+    await helpers.highlightOff(await dynamicLoadingPage.txtHelloWorld);
   });
 
   it("Chapter 5: Self-healing Link", async () => {
@@ -87,27 +61,21 @@ describe("My Login application", () => {
     );
   });
 
-  fit("Chapter 6: setValue Wrapper", async () => {
+
+  it("Chapter 5: Self-healing Link", async () => {
     // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
-
-    console.log(await helpers.getToday()); // returns current date in MM-dd-yyyy format
-    console.log(await helpers.getToday(7, "d/M/yyyy")); // returns current date plus 5 days in d/M/yyyy format
-    console.log(await helpers.getToday(-30, "yyyy/MM/dd")); // returns current date minus 3 days in yyyy/MM/dd format
-
     await LoginPage.open();
-    await LoginPage.loginSetValue("tomsmith", "SuperSecretPassword!");
 
-    await LoginPage.open("key_presses");
-    await KeyPressesPage.enterDateTags();
+    await LoginPage.loginOld("tomsmith", "SuperSecretPassword!");
+    await expect(SecurePage.flashAlert).toBeExisting();
+    await expect(SecurePage.flashAlert).toHaveTextContaining(
+      "You logged into a secure area!"
+    );
   });
 
   it("should login with valid credentials", async () => {
     // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
     await LoginPage.open();
-    // Chapter 3
-    // console.log (`Entering password`) // Intrinsic log
-    // await global.log (`Entering password`) // Custom log
-
     await LoginPage.login("tomsmith", "SuperSecretPassword!");
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining(
