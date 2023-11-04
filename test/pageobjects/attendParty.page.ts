4366e582a065570388f11afd79ea2442a157c406
Paul Grossman <thedarkartswizard@gmail.com>
2023-11-04 18:35:47 -0500

    added halloweenParty pages

---
+++ test/pageobjects/attendParty.page.ts
@@ -0,0 +1,51 @@
+// halloweenAttendPartyPage
+
+import Page from './page.js'; 
+import * as helpers from "../../helpers/helpers.js"; 
+
+/** 
+ * sub page with selectors for a specific page 
+ */ 
+class AttendPartyPage extends Page { 
+    /** 
+     * define selectors using getter methods 
+     */ 
+    public get hostParty () { 
+        return $(`//a[contains(normalize-space(),'ost')]`); 
+    } 
+
+    public get attendParty () { 
+        return $(`//a[contains(normalize-space(),'ttend')]`); 
+    } 
+    
+    public async build (testdata) { 
+
+        let success:boolean = true; 
+      
+       
+         // Is this the page to process?   	 
+      
+         if (await ASB.get("page") === "attend-a-party") { 
+      
+           if (testdata.HostOrAttend.toLowerCase() === `host`){ 
+      
+              success = await  
+                 helpers.clickAdv(await this.hostParty); 
+      
+           }else{ 
+      
+              success = await  
+                 helpers.clickAdv(await this.attendParty); 
+      
+              } 
+      
+            } 
+          } 
+      
+         return success; 
+      
+      } 
+      
+       
+      
+      export default new AttendPartyPage(); 
--- test/specs/ch14.ts
+++ test/specs/ch14.ts
@@ -7,9 +7,8 @@ describe("Ch14: State-drive Automation", () => {
   it("should loop around until the final page is found", async () => {
     await LoginPage.open();
 
-    await LoginPage.login(Data.userData.username, Data.userData.password);
-    await helpers.expectAdv(SecurePage.flashAlert, 'exists', null);
-    await helpers.expectAdv(SecurePage.flashAlert, 'toHaveTextContaining', (["You logged into a secure area!"]));
+    helpers.partyPath();
+
   });
 
 
