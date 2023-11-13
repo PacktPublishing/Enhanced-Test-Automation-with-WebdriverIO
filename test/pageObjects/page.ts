af5604a3a730e4da64386b22a93c3c0801cd59f4
Paul Grossman <thedarkartswizard@gmail.com>
2023-11-12 23:02:12 -0600

    Page reports URL in box

--- test/pageObjects/page.ts
+++ test/pageObjects/page.ts
@@ -3,15 +3,25 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
-
+import allureReporter from '@wdio/allure-reporter';
+import { baseUrl } from '../../wdio.shared.conf';
 export default class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     public async open (path: string) {
-        global.log(`Opening browser to https://the-internet.herokuapp.com/${path}`)
+        allureReporter.addAttachment('Navigating to url', path, 'string');
+        //global.log(`Opening browser to https://the-internet.herokuapp.com/${path}`)
         // return await browser.url(`https://the-internet.herokuapp.com/${path}`)
+        
+        const message = `Opening URL: ${baseUrl}/${path}`;
+        const line = '-'.repeat(message.length);
+        
+        global.log(line);
+        global.log(message);
+        global.log(line);
+        
         return await browser.url(`/${path}`)
     }
 
