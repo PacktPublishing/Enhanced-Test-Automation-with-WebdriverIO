550bca0cf8a856c58925ce7376d2690842be8412
Paul Grossman <thedarkartswizard@gmail.com>
2023-11-24 09:39:15 -0600

    Enhanced test decriptions

--- test/pageObjects/dynamicLoading.page.ts
+++ test/pageObjects/dynamicLoading.page.ts
@@ -13,6 +13,10 @@ class DynamicLoadingPage extends Page {
     return $(`//a[contains(text(),'hidden')]`);
   }
 
+  public get btnRenderedElement() {
+    return $(`//a[contains(text(),'rendered')]`);
+  }
+
   public get btnStart() {
     return $(`//button[normalize-space()='Start']`);
   }
@@ -27,10 +31,30 @@ verifyHelloWorld();
    * a method to encapsule automation code to interact with the page
    * e.g. to login using username and password
    */
-  public async navToSpinner() {
+  public async clickStartWithHiddenElementAndSpinner() {
+    // Click Start button that navigates to page with hidden "Hello world" element and spinner 
     await helpers.clickAdv(await this.btnHiddenElement);
+
+    // clickAdv waits for the dynamically waits for spinner to disappear
+    // It highlights the spinner while it is waiting.
     await helpers.clickAdv(await this.btnStart);
+  
   }
+
+  /**
+   * a method to encapsule automation code to interact with the page
+   * e.g. to login using username and password
+   */
+  public async clickStartWithRenderedElementAndSpinner() {
+    // Click Start button that navigates to page with "Hello world" element rendered after spinner    
+    await helpers.clickAdv(await this.btnRenderedElement);
+
+    // clickAdv waits for the to disappear before rendering "Hello World" on the page.
+    // It highlights the spinner while it is waiting.
+    await helpers.clickAdv(await this.btnStart);
+  
+  }
+
   /**
    * overwrite specific options to adapt it to page object
    */
--- test/specs/ch5.ts
+++ test/specs/ch5.ts
@@ -4,28 +4,60 @@ import LandingPage from "../pageObjects/landing.page";
 import * as helpers from "../../helpers/helpers";
 import dynamicLoadingPage from "../pageObjects/dynamicLoading.page";
 
-describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
+// describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
 
-  it("Chapter 5: navToTypes - autoscroll", async () => {
-    // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
-    await LandingPage.open();
-    await LandingPage.navToTypos();
-    // await expect(typosPage.typoText).toHaveTextContaining(   'You logged into a secure area!');
-  });
+//   it("Chapter 5: navToTypos - autoscroll", async () => {
+//     // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
+//     await LandingPage.open();
+//     await LandingPage.navToTypos();
+//     // await expect(typosPage.typoText).toHaveTextContaining(   'You logged into a secure area!');
+//   });
+// });
 
-  it("Chapter 5: navToDynamicLoading - spinner", async () => {
-    // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
+describe("Chapter 5: Buring wait time of Dynamically Loaded elements with spinner check embedded in clickAdv()", () => {
+  // Example 1: Element on page that is hidden
+  it(`Clicks Start with clickadv which waits for spinner before hidden "Hello World" is displayed`, async () => {
     await LandingPage.open();
     await LandingPage.navToDynamicLoading();
-    await dynamicLoadingPage.navToSpinner();
+    
+    // Click Hidden Element Start which displays a spinner with hidden "Hello World"
+    // Flashes the spinner while waiting to disappear without additional wait commands
+    await dynamicLoadingPage.clickStartWithHiddenElementAndSpinner();
 
+    //Verify the hidden Hello World text appears after the spinner disappeared
     await expect(dynamicLoadingPage.txtHelloWorld).toHaveTextContaining(
       "Hello World"
     );
+
+    //Highlight the Hello World text
     await helpers.highlightOn(await dynamicLoadingPage.txtHelloWorld);
     await helpers.pause(5000);
     await helpers.highlightOff(await dynamicLoadingPage.txtHelloWorld);
   });
+
+  // Example 2: Element rendered after the fact
+  it(`Clicks Start with clickadv which waits for spinner before "Hello World" is rendered`, async () => {
+    await LandingPage.open();
+    await LandingPage.navToDynamicLoading();
+    
+    // Click Hidden Element Start which displays a spinner before rendereing "Hello World"
+    // Flashes the spinner while waiting to disappear without additional wait commands
+    await dynamicLoadingPage.clickStartWithRenderedElementAndSpinner();
+
+    //Verify the Hello World text was rendered after the spinner disappeared
+    await expect(dynamicLoadingPage.txtHelloWorld).toHaveTextContaining(
+      "Hello World"
+    );
+
+    //Highlight the Hello World text for 5 seconds
+    await helpers.highlightOn(await dynamicLoadingPage.txtHelloWorld);
+    await helpers.pause(5000);
+    await helpers.highlightOff(await dynamicLoadingPage.txtHelloWorld);
+  });
+
+});
+
+describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
 
   it("should login with valid credentials", async () => {
     // await helpers.log(Promise) // Unit test log returns warning when anything but string is passsed
@@ -37,6 +69,10 @@ describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll
     );
   });
 
+});
+
+fdescribe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
+
   it("Chapter 5: Self-healing Link", async () => {
     await LoginPage.open();
     await LoginPage.loginOld("tomsmith", "SuperSecretPassword!");
@@ -46,6 +82,10 @@ describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll
     );
   });
 
+});
+
+describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
+
   it("Chapter 5: The last element in loginFailLast() is a bogus element", async () => {
     await LoginPage.open();
     await LoginPage.loginFailLast("tomsmith", "SuperSecretPassword!");
@@ -54,6 +94,9 @@ describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll
         "You logged into a secure area!"
     );
   });
+});
+
+describe("Chapter 5: Intentional Fail Last clickAdv with pageSync and autoscroll", () => {
 
   it("Chapter 5: Fail First clickAdv with pageSync and autoscroll", async () => {
     await LoginPage.open();
