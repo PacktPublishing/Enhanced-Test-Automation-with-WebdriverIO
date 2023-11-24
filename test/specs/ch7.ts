f1ff2646f4e4c79f65065a8919be2b7d8c3fed54
Paul Grossman <thedarkartswizard@gmail.com>
2023-11-24 04:17:07 -0600

    Updated Ch7 to use Telerik

--- test/specs/ch7.ts
+++ test/specs/ch7.ts
@@ -1,53 +1,19 @@
-import WebListPage from "../pageObjects/weblist.page";
+
+import TelerikPage from "../pageObjects/telerik.page";
+
 describe("The select Wrapper: Selecting from List and Combobox", () => {
 
     it("Chapter 7: Select from a Combo Box", async () => {
         let elem;
-        // Navigate to a sample weblist
-        await WebListPage.open("components/dropdowns/combobox/");
-        // this handles the cookie popup if required
-        elem = await browser.$('[id="onetrust-accept-btn-handler"]')
-        if(await elem.isExisting() === true){
-            await elem.click();
-        }
-        await browser.pause(500);
-        // This scrolls the page
-        elem = await browser.$('[class="link-sc-6kfngc-0 HwYv"]')
-        await elem.scrollIntoView();
-        await browser.pause(500);
-        // This gets you into the iFrame
-        const iframe = await browser.findElement('css selector', 'iframe');
-        await browser.switchToFrame(iframe);
-        elem = await browser.$('[class="k-input-inner"]');
-        await elem.scrollIntoView();
-        await browser.pause(500);
-        await elem.setValue('Cricket');
-        await browser.keys('Enter');
-        await browser.pause(3000);
+        // Navigate to a sample combobox weblist
+        await TelerikPage.open("components/dropdowns/combobox/");
+        await TelerikPage.selectSport("Cricket");
     });
 
     it("Chapter 7: Selecting from a AutoComplete list", async () => {
         let elem;
         // Navigate to a sample weblist
-        await WebListPage.open("components/dropdowns/autocomplete/");
-        // this handles the cookie popup if required
-        elem = await browser.$('[id="onetrust-accept-btn-handler"]')
-        if(await elem.isExisting() === true){
-            await elem.click();
-        }
-        await browser.pause(500);
-        // This scrolls the page
-        elem = await browser.$('[class="link-sc-6kfngc-0 HwYv"]')
-        await elem.scrollIntoView();
-        await browser.pause(500);
-        // This gets you into the iFrame
-        const iframe = await browser.findElement('css selector', 'iframe');
-        await browser.switchToFrame(iframe);
-        elem = await browser.$('[class="k-input-inner"]');
-        await elem.scrollIntoView();
-        await elem.doubleClick();
-        await elem.setValue('Belg');
-        await browser.keys('Enter');
-        await browser.pause(10000);
+        await TelerikPage.open("components/dropdowns/autocomplete/");
+        await TelerikPage.selectCountry("Denmark");
     });
 });
