d67bdff1caca705df6621dc335a6f8ce3b5ef8b2
Paul Grossman <thedarkartswizard@gmail.com>
2023-12-03 13:43:40 -0600

    Added the initial non-framework example created by install

--- test/specs/ch2.ts
+++ test/specs/ch2.ts
@@ -1,14 +1,31 @@
 import LoginPage from "../pageObjects/login.page";
 import SecurePage from "../pageObjects/secure.page";
 
+  // This test uses almost no custom framework except the wdio.shared.conf.ts custom commands
+  describe("A near mortal WebdriverIO script", () => {
+    it("should login with valid credentials", async () => {
+      await LoginPage.open();
+  
+      // loginWdio is a function that uses only the WebdriverIO API
+      await LoginPage.login("tomsmith", "SuperSecretPassword!");
+      
+      await expect(SecurePage.flashAlert).toBeExisting();
+      await expect(SecurePage.flashAlert).toHaveTextContaining(
+        "You logged into a secure area!"
+      );
+    });
+  })
+
+// This test uses the advanced library
 describe("Ch2: Fortress of Solitude", () => {
-  it("should login with valid credentials", async () => {
+  it("should login with valid credentials with details", async () => {
     await LoginPage.open();
 
-    await LoginPage.login("tomsmith", "SuperSecretPassword!");
+    // login is a function that uses the wrapper methods
+    await LoginPage.loginAdv("tomsmith", "SuperSecretPassword!");
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining(
       "You logged into a secure area!"
-    );
+    );  
   });
 });
